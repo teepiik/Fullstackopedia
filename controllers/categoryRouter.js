ec2feec2a5ac90ddb969485c5ca5c32052c22cb7
teepiik <teemu.piik@gmail.com>
2020-02-01 18:02:06 +0200

    category router updated

--- controllers/categoryRouter.js
+++ controllers/categoryRouter.js
@@ -6,6 +6,29 @@ categoryRouter.get('/', async (req, res) => {
     res.json(categories.map(category => category.toJSON()))
 })
 
+categoryRouter.get('/:id', async (req, res, next) => {
+    try {
+        const category = await Category.findById(req.params.id)
+        res.status(200).json(category.toJSON())
+    } catch (error) {
+        next(error)
+    }
+})
+
+categoryRouter.put('/:id', async (req, res, next) => {
+    const body = req.body
+    try {
+        const category = {
+            categoryName: body.categoryName
+        }
+        const updatedCategory = await Category.findByIdAndUpdate(req.params.id, category, { new:true })
+        res.status(200).json(updatedCategory.toJSON())
+
+    } catch (error) {
+        next(error)
+    }
+})
+
 categoryRouter.post('/', async (req, res, next) => {
     const body = req.body
     try {
@@ -16,6 +39,15 @@ categoryRouter.post('/', async (req, res, next) => {
         res.status(201).json(savedCategory.toJSON())
 
     } catch(error) {
+        next(error)
+    }
+})
+
+categoryRouter.delete('/:id', async (req, res, next) => {
+    try {
+        await Category.findByIdAndDelete(req.params.id)
+        res.status(204).end()
+    } catch (error) {
         next(error)
     }
 })
