6c9cf34787893e1b1948ce041114a9062a7e0964
teepiik <teemu.piik@gmail.com>
2020-03-25 21:19:54 +0200

    gamequiz router

--- controllers/gameQuestionRouter.js
+++ controllers/gameQuestionRouter.js
@@ -1,5 +1,6 @@
 const gameQuestionRouter = require('express').Router()
 const GameQuestion = require('../models/GameQuestion')
+const jwt = require('jsonwebtoken')
 
 gameQuestionRouter.get('/', async (req, res) => {
     const questions = await GameQuestion.find({})
@@ -19,6 +20,60 @@ gameQuestionRouter.get('/:id', async (req, res, next) => {
     }
 })
 
-// TODO add Post, Put, Del
+gameQuestionRouter.put('/:id', async (req, res, next) => {
+    const body = req.body
+    try {
+        const decodedToken = jwt.verify(req.token, process.env.SECRET)
+        if(!req.token || !decodedToken.id) {
+            return res.status(401).json({ error: 'Token missing or invalid.' })
+        }
+
+        const objForUpdate = {
+            question: body.question,
+            choices: body.choices,
+            correctAnswer: body.correctAnswer
+        }
+
+        const updatedGameQuestion = await GameQuestion.findByIdAndUpdate(req.params.id, objForUpdate, { new:true })
+        res.status(200).json(updatedGameQuestion.toJSON())
+    } catch (error) {
+        next(error)
+    }
+})
+
+gameQuestionRouter.post('/', async (req, res, next) => {
+    const body = req.body
+    try {
+        const decodedToken = jwt.verify(req.token, process.env.SECRET)
+        if(!req.token || !decodedToken.id) {
+            return res.status(401).json({ error: 'Token missing or invalid.' })
+        }
+
+        const newGameQuestion = new GameQuestion({
+            question: body.question,
+            choices: body.choices,
+            correctAnswer: body.correctAnswer
+        })
+
+        const savedGameQuestion = await newGameQuestion.save()
+        res.status(201).json(savedGameQuestion.toJSON())
+    } catch (error) {
+        next(error)
+    }
+})
+
+gameQuestionRouter.delete('/:id', async (req, res, next) => {
+    try {
+        const decodedToken = jwt.verify(req.token, process.env.SECRET)
+        if(!req.token || !decodedToken.id) {
+            return res.status(401).json({ error: 'Token missing or invalid.' })
+        }
+
+        await GameQuestion.findByIdAndRemove(req.params.id)
+        res.status(204).end()
+    } catch (error) {
+        next(error)
+    }
+})
 
 module.exports = gameQuestionRouter
--- controllers/gameRouter.js
+++ controllers/gameRouter.js
@@ -1,3 +1,12 @@
 const gameRouter = require('express').Router()
 const GameQuestion = require('../models/GameQuestion')
 const User = require('../models/User')
+
+// get right question for User id
+gameRouter.get('/:id')
+
+// handle answer for User id
+gameRouter.post('/:id')
+
+
+module.exports = gameRouter
